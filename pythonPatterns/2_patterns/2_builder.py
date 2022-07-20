4eb6308cea72f37430ebbb0487437a6af957b8cd
Daniel Pineda <danielpinedaj@gmail.com>
2022-07-19 22:12:03 -0500

    Builder Facets with multiple child Builders

--- pythonPatterns/2_patterns/2_builder.py
+++ pythonPatterns/2_patterns/2_builder.py
@@ -91,3 +91,94 @@ builder.add_child_fluent("li", "hello")\
     .add_child_fluent("li", "world")
 print("Fluent Builder:")
 print(builder)
+
+
+# Builder Facets:
+# ----------------------------------------------------------------
+# -
+
+
+class Person:
+    def __init__(self):
+        # address
+        self.street_address = None
+        self.potscode = None
+        self.city = None
+
+        # employment
+        self.company_name = None
+        self.position = None
+        self.annual_income = None
+
+    def __str__(self):
+        return (
+            f"Address: {self.street_address}, {self.potscode}, {self.city} "
+            f"Employed at {self.company_name} as a {self.position} "
+            f"earning {self.annual_income}"
+        )
+
+
+class PersonBuilder:
+    def __init__(self, person=Person()):
+        self.person = person
+
+    @property
+    def works(self):
+        return PersonJobBuilder(self.person)
+
+    @property
+    def lives(self):
+        return PersonAddressBuilder(self.person)
+
+    def build(self):
+        return self.person
+
+
+class PersonJobBuilder(PersonBuilder):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def at(self, company_name):
+        self.person.company_name = company_name
+        return self
+
+    def as_a(self, position):
+        self.person.position = position
+        return self
+
+    def earning(self, annual_income):
+        self.person.annual_income = annual_income
+        return self
+
+
+class PersonAddressBuilder(PersonBuilder):
+    def __init__(self, person):
+        super().__init__(person)
+
+    def at(self, street_address):
+        self.person.street_address = street_address
+        return self
+
+    def with_postcode(self, potscode):
+        self.person.potscode = potscode
+        return self
+
+    def in_city(self, city):
+        self.person.city = city
+        return self
+
+
+pb = PersonBuilder()
+person = (
+    pb
+    .lives
+    .at("123 London Road")
+    .in_city("London")
+    .with_postcode("SW12BC")
+    .works
+    .at("Fabrikam")
+    .as_a("Engineer")
+    .earning(123000)
+    .build()
+)
+print(person)
